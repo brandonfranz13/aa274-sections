8dfa353a4fb37e20caa539b06131f421ccbbfe69
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-05 17:59:52 -0500

    wip

--- section3/code/odom_subscriber.py
+++ section3/code/odom_subscriber.py
@@ -1,12 +1,14 @@
 import rospy
+from geometry_msgs import Twist
 
-
-def callback(data):
-    pass
+def callback(cmd_vel):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", cmd_vel)
 
 
 def subscriber():
-    pass
+    rospy.init_node('odom_sub', anonymous=True)
+    rospy.Subscriber("cmd_vel", Twist, callback(cmd_vel))
+    rospy.spin()
 
 
 if __name__ == '__main__':
--- section3/code/vel_publisher.py
+++ section3/code/vel_publisher.py
@@ -1,8 +1,16 @@
 import rospy
-
+from geometry_msgs import Twist
 
 def publisher():
-    pass
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    rospy.init_node('cmd_vel', anonymous=True)
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        my_message = Twist()
+        my_message.linear = (0, 0, 0)
+        my_message.angular = (0, 0, 0)
+        pub.publish(my_message)
+        rate.sleep()
 
 
 if __name__ == '__main__':
